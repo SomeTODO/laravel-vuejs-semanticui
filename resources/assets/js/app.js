b71523e33747efbe14e13db08a749ca2b6eeb2dc
onetodo <bign.2006@gmail.com>
2016-12-20 17:46:11 +0800

    delete login

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -14,7 +14,6 @@ require('./bootstrap');
  */
 
 Vue.component('example', require('./components/Example.vue'));
-Vue.component('login', require('./components/Login.vue'));
 
 const app = new Vue({
     el: '#app'
